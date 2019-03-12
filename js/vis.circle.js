d3fa87dea6905571bb303d7dcc8cc1adf69f6c5c
Russell Cheung <thechunk@gmail.com>
2019-03-12 21:30:34 +0800

    vis: refactor and add some reference lines

--- js/vis.circle.js
+++ js/vis.circle.js
@@ -50,7 +50,6 @@ class CircleVis {
         return {x, y};
     }
 
-
     static drawDots(d3, vis, data, x, y, rad) {
         const c = vis.selectAll('circle')
             .data(data)
@@ -73,62 +72,83 @@ class CircleVis {
                     c.on('mouseover', function(d, i) {
                         CircleVis.handleDotMouseover(this, d3, vis, data, x, y, rad, d, i);
                     });
-                    c.on('mouseout', function(d, i) {
-                        d3.select(this).transition()
-                            .ease(d3.easeElastic.period(1))
-                            .duration(500)
-                            .attr('r', d.r);
-                        d3.selectAll(`#line-${i}`)
-                            .transition()
-                            .style('opacity', 0)
-                            .on('end', function() {
-                                d3.select(this).remove()
-                            });
-                        d3.selectAll(`#desc-${i}`)
-                            .transition()
-                            .style('opacity', 0)
-                            .on('end', function() {
-                                d3.select(this).remove();
-                            });
-                    });
+                    c.on('mouseout', CircleVis.handleDotMouseout);
                 }
             });
 
     }
 
-    static handleDotMouseover(target, d3, vis, data, x, y, rad, d, i) {
+    static handleDotMouseover(target, d3, vis, data, x, y, r, d, i) {
         d3.select(target).transition()
             .ease(d3.easeElastic.period(1))
             .duration(500)
             .attr('r', d => d.r * 1.5);
         if (i < data.length - 1) {
-            vis.append('line')
-                .style('stroke', 'black')
-                .style('opacity', 0)
-                .attr('id', `line-${i}`)
-                .attr('x1', rad)
-                .attr('y1', rad)
-                .attr('x2', x(d.x))
-                .attr('y2', y(d.y))
-                .transition()
-                .style('opacity', 1);
-            const descGroup = vis.append('g')
-                .style('opacity', 0)
-                .attr('transform', `rotate(
-                                        ${Math.atan2(y(d.y) - rad, x(d.x) - rad) * 180 / Math.PI}
-                                        ${(rad + x(d.x)) / 2}
-                                        ${(rad + y(d.y)) / 2}
-                                    )`)
-                .attr('id', `desc-${i}`);
-            const descText = descGroup.append('text');
-            descText
-                .attr('x', (rad + x(d.x)) / 2)
-                .attr('y', (rad + y(d.y)) / 2)
-                .text('hello');
-            descGroup.transition()
-                .style('opacity', 1);
+            this.drawOverlay(vis, x, y, r, d, i);
         }
-    };
+    }
+
+    static handleDotMouseout(d, i) {
+        d3.select(this).transition()
+            .ease(d3.easeElastic.period(1))
+            .duration(500)
+            .attr('r', d.r);
+        d3.selectAll(`#over-${i}`)
+            .transition()
+            .style('opacity', 0)
+            .on('end', function() {
+                d3.select(this).remove()
+            });
+        d3.selectAll(`#desc-${i}`)
+            .transition()
+            .style('opacity', 0)
+            .on('end', function() {
+                d3.select(this).remove();
+            });
+    }
+
+    static drawOverlay(vis, x, y, r, d, i) {
+        const xDx = x(d.x);
+        const yDy = y(d.y);
+        const data = [
+            {x1: r, y1: r, x2: xDx, y2: yDy, stroke: 'black'},
+            {x1: r, y1: r, x2: xDx, y2: r, stroke: 'red'},
+            {x1: xDx, y1: r, x2: xDx, y2: yDy, stroke: 'blue'},
+        ];
+        const overlayGroup = vis.append('g')
+            .attr('id', `over-${i}`);
+        overlayGroup
+            .selectAll('line')
+            .data(data)
+            .enter().append('line')
+            .style('stroke', d => d.stroke)
+            .attr('x1', d => d.x1)
+            .attr('y1', d => d.y1)
+            .attr('x2', d => d.x2)
+            .attr('y2', d => d.y2);
+        const descGroup = overlayGroup.append('g')
+            .attr('transform', `rotate(
+                                        ${this.radToDeg(Math.atan2(y(d.y) - r, x(d.x) - r))}
+                                        ${(r + x(d.x)) / 2}
+                                        ${(r + y(d.y)) / 2}
+                                    )`)
+            .attr('id', `desc-${i}`);
+        const descText = descGroup.append('text');
+        descText
+            .attr('x', (r + x(d.x)) / 2)
+            .attr('y', (r + y(d.y)) / 2)
+            .text('hello');
+
+        overlayGroup
+            .style('opacity', 0)
+            .transition()
+            .style('opacity', 1);
+        return overlayGroup;
+    }
+
+    static radToDeg(rad) {
+        return rad * 180 / Math.PI;
+    }
 
     main() {
         const vis = CircleVis.drawContainer(this.d3, this.width, this.height);
